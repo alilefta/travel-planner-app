c12d42549eeeafe778f418334d6d46a53f7e548a
alilefta <alilefta95@gmail.com>
2021-05-01 14:45:14 +0300

    fix commonJS issue with bootstrap

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,6 @@
 import './styles/style.scss'
-import 'bootstrap/js/dist/modal'
 import {handleSubmit} from './js/handleSubmit';
+import './js/bootstrap'
 
 export{
     handleSubmit
---
+++ src/client/js/bootstrap.js
@@ -0,0 +1,5 @@
+const bootstrap = require('bootstrap');
+
+export{
+    bootstrap
+}
